b2dfd4a19143c7610c80a8cbdae91215488ed369
tapciganin <jitza_nest@yahoo.com>
2016-10-19 18:53:26 +0300

    Exercises on ADO.NET basics

---
+++ Databases Fundamentals/Databases Advanced/01. InitialSetup/02. GetVillainsNames/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.GetVillainsNames
+{
+    using System;
+    using System.Data.SqlClient;
+
+    class Program
+    {
+        static void Main()
+        {
+            string connectionString = "Server=6pekIV\\SQLEXPRESS; Database=Minions; Trusted_Connection = true";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string groupByVillainName = @"SELECT  v.Name,
+		                                              COUNT(*) AS CountOfMinions
+                                             FROM VillainsMinions AS vm
+	                                             INNER JOIN Villains AS v
+		                                             ON vm.VillainID = v.VillainID
+                                             GROUP BY v.Name
+                                             HAVING COUNT(*) >3
+                                             ORDER BY CountOfMinions DESC";
+                SqlCommand command = new SqlCommand(groupByVillainName, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write($"{ reader[i]} ");
+                    }
+                    Console.WriteLine();
+                }
+                
+            }
+        }
+    }
+}
