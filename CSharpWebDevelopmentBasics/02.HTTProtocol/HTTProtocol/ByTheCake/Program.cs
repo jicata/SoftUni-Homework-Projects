de869000727aef6b35f478b6e7d1b4646b41cc14
tapciganin <jitza_nest@yahoo.com>
2017-01-26 22:13:55 +0200

    HTTProtocol and CGI playthings

---
+++ CSharpWebDevelopmentBasics/02.HTTProtocol/HTTProtocol/ByTheCake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByTheCake
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string byTheCakeHtml =
+                File.ReadAllText(
+                    @"D:\xampp\cgi-bin\ByTheCakeIndex.html");
+            Console.Write("Content-Type: text/html image/jpeg \n\n");                           
+            Console.Write(byTheCakeHtml);
+        }
+    }
+}
---
+++ CSharpWebDevelopmentBasics/02.HTTProtocol/HTTProtocol/ByTheCakeAddCake/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ByTheCakeAddCake
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string dbpath = @"D:\xampp\htdocs\cakes.csv";
+            string byTheCakeHtml =
+               File.ReadAllText(
+                   @"D:\xampp\cgi-bin\AddCake.html");
+            Console.Write("Content-Type: text/html image/jpeg \n\n");
+            Console.Write(byTheCakeHtml);
+            string postContent = Console.ReadLine();
+            if (postContent != null)
+            {
+                string[] firstSplit = postContent.Split('&');
+                string name = firstSplit[0].Split('=')[1];
+                string price = firstSplit[1].Split('=')[1];
+                string formatted = $"Name:{name}, Price:{price} \n";
+                File.AppendAllText(dbpath, formatted);
+                Console.WriteLine($"{name} ${price}");
+            }
+        }
+    }
+}
