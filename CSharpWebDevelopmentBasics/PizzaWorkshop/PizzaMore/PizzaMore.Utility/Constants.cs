cf5517ebd5848b07968a4b186e3d81ae8167061d
jicata <jitza_nest@yahoo.com>
2017-02-07 17:57:34 +0200

    cGGi....

--- CSharpWebDevelopmentBasics/PizzaWorkshop/PizzaMore/PizzaMore.Utility/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PizzaMore.Utility
-{
-    public static class Constants
-    {
-        public const string LogPath = @"D:\xampp\cgi-bin\cgi-pizza\PizzaLog.txt";
-
-        public const string ContentType = "Content-Type: text/html \n\n";
-    }
-}
