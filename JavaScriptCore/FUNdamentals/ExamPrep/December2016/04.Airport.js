776b2ef3a052e440359560f31c5b2b71b2ea7a62
jicata <jitza_nest@yahoo.com>
2017-06-02 23:37:45 +0300

    JS exam prep part II

--- JavaScriptCore/FUNdamentals/ExamPrep/December2016/04.Airport.js
+++ JavaScriptCore/FUNdamentals/ExamPrep/December2016/04.Airport.js
@@ -1,37 +1,67 @@
 /*jshint esversion: 6 */
 function airport(data) {
-    let planes = [];
-    let townPlane = new Map();
+    let planes = new Map();
+    let townsPlanes = new Map();
 
     for (let airplaneInfo of data) {
         let airplaneDetails = airplaneInfo.split(' ');
 
-        let planeId = airplaneDetails[0];
-        let town = airplaneDetails[1];
+        let planeId = airplaneDetails[0].trim();
+        let town = airplaneDetails[1].trim();
         let passengers = Number(airplaneDetails[2]);
-        let action = airplaneDetails[3];
+        let action = airplaneDetails[3].trim();
 
-        let indexOfPlane = planes.indexOf(planeId);
-        if (indexOfPlane == -1 && action == 'land') {
-            planes.push(planeId);
-        }
-        else {
-            if (action == "depart") {
-                planes.splice(indexOfPlane, 1);
+        let planeInfo = {
+            passengers: passengers,
+            action: action
+        };
+
+        if (validatePlane(planeId, planes, action)) {
+            planes.set(planeId,action);
+            if (!townsPlanes.has(town)) {
+                let planesInTown = new Map();
+                planesInTown.set(planeId, planeInfo);
+                townsPlanes.set(town, planesInTown);
+            }
+            else {
+                planesInTown = townsPlanes.get(town);
+                let existingPlane = planesInTown.get(planeId)
+
+                if(typeof existingPlane !== 'undefined'){
+                    
+                }
+                planesInTown.set(planeId, planeInfo);
+                townsPlanes.set(town, planesInTown);
             }
         }
-
-        if (!townPlane.has(town) && action == "land") {
-            let plane = {
-                id: planeId,
-                passengers: passengers,
-                action: action
-            };
-            townPlane.set(town, new Set());
+    }
 
 
+    function validatePlane(planeId, planes, newAction) {
+        "use strict";
+        if (!planes.has(planeId) && newAction == 'land') {
+            planes.set(planeId, newAction);
+            return true;
+        }
+        let oldAction = planes.get(planeId);
+        if (oldAction == 'depart') {
+            return newAction == 'land';
+        }
+        if (oldAction == 'land') {
+            return newAction != 'land';
         }
     }
+    console.log("Planes left:");
+    for(let [plane,action] of planes){
+        if(action!='depart'){
+            console.log(`- ${plane}`)
+        }
+    }
+    for(let [town,planes] of townsPlanes){
+        console.log(town);
+    }
+    //console.log(planes);
+    console.log(townsPlanes);
 }
 airport([
     "Boeing474 Madrid 300 land",
