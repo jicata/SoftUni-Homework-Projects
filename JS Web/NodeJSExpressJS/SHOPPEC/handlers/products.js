3196c668a0e600f53c1aac762ca564e1ae9d52e0
jicata <jitza_nest@yahoo.com>
2017-10-04 20:08:45 +0300

    Express JS - Lab part II

--- JS Web/NodeJSExpressJS/SHOPPEC/handlers/products.js
+++ JS Web/NodeJSExpressJS/SHOPPEC/handlers/products.js
@@ -3,10 +3,12 @@ const database = require('../config/database')
 const fs = require('fs')
 const path = require('path')
 const qs = require('querystring')
+const multiparty = require('multiparty')
+const shortid = require('shortid')
 
 module.exports = (req, res) => {
   req.pathname = req.pathname || url.parse(req.url).pathname
-  if (req.pathname === '/product/add' && req.method == 'GET') {
+  if (req.pathname === '/product/add' && req.method === 'GET') {
     let filePath = path.normalize(
             path.join(__dirname, '../views/products/add.html')
         )
@@ -23,6 +25,52 @@ module.exports = (req, res) => {
       res.end(data)
     })
   } else if (req.pathname === '/product/add' && req.method === 'POST') {
+    let form = new multiparty.Form()
+
+    form.on('part', (part) => {
+      if (part.filename) {
+        let dataString = ''
+
+        part.setEncoding('binary')
+        part.on('data', (data) => {
+          dataString += data
+        })
+        part.on('end', () => {
+          let fileName = shortid.generate()
+          let filePath = `../content/images/${fileName}`
+          
+          product.image = filePath
+          fs.writeFile(
+            `${filePath}`, dataString, {encoding: 'ascii'}, (err) => {
+              if (err) {
+                console.log(err)
+                return
+              } else {
+                part.setEncoding('utf-8')
+                let field = ''
+                part.on('data', (data) => {
+                  field += data
+                })
+                              
+                part.on('end', () => {
+                  product[part.name] = field
+                })
+              }
+            }
+          )
+        })
+      }
+      form.on('close', () => {
+        database.products.add(product)
+        res.writeHead(302, {
+          Location: '/'
+        })
+
+        res.end()
+      })
+      form.parse(req)
+    })
+
     let dataString = ''
     req.on('data', (data) => {
       dataString += data
